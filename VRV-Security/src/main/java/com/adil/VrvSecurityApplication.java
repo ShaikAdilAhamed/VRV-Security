eded949fb6aaecfc69bf2db6aa1fddc7da4c4845
Shaik Adil Ahamed <s.adilahmed786@gmail.com>
2024-12-25 15:22:12 +0530

    VRV-Security Repository setup part-1

---
+++ VRV-Security/src/main/java/com/adil/VrvSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.adil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VrvSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VrvSecurityApplication.class, args);
+	}
+
+}
---
+++ VRV-Security/src/main/java/com/tecxter/VrvSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.tecxter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VrvSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VrvSecurityApplication.class, args);
+	}
+
+}
